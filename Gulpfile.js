9162b6b9ebc8cf0e4cb9448eb8878d5e71f00fd1
Anton Bayer <anton.bayer@gmx.at>
2016-05-13 15:52:01 +0200

    Update Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -96,33 +96,43 @@ gulp.task('jenkinsDownload', function() {
 	function getAllJobs() {
 
 		jenkins.job.list(function(err, data) {
+			
 			if (err){
 				return console.log(err);
 			}
-			for (var i = 0;i<data.length;i++) {
-				getConfigXml(data[i].name);		
-			}  
+			
+			if (data.length) {
+				var callback = function(i) {
+					i++;
+					if (i < data.length) {
+						getConfigXml(data[i].name, i, callback);
+					}
+				};
+				getConfigXml(data[0].name, 0, callback);		
+			}
 		});
 	}
 
-	function getConfigXml(name){
+	function getConfigXml(name, index, callback){
 		
 		jenkins.job.config(name, function(err, data) {
 			if (err){
 				return console.log(name + " " + err);
 			}
 			
-			saveJobConfigToFile(name, data);
+			saveJobConfigToFile(name, data, index, callback);
 		});
 	}
 
-	function saveJobConfigToFile(name, content) {
+	function saveJobConfigToFile(name, content, index, callback) {
 		
 		fs.writeFile("jobs-download/" + name + ".xml", content, function(err) {
 			if(err) {
 				return console.log(name + " " + err);
 			}
 			console.log(name + " saved");
+			
+			callback(index);
 		});
 	}
 
